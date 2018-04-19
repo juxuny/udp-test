d3ea27ad8eeb237c25e2d7ad200ce2175bd75857
Juxuny Wu <juxuny@gmail.com>
2018-04-19 15:02:18 +0800

    init

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"os"
+)
+
+
+
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("not enough arguments")
+		return
+	}
+	addr := os.Args[1]
+	port := os.Args[2]
+	fmt.Println(fmt.Sprintf("%s:%s", addr, port))
+	udpAddr, e := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%s", addr, port))
+	if e != nil {
+		panic(e)
+		return
+	}
+	ln, e := net.ListenUDP("udp", udpAddr)
+	go func () {
+		for {
+			var buf = make([]byte, 1000)
+			n, cAddr, e := ln.ReadFrom(buf)
+			if e != nil {
+				fmt.Println(e)
+				break
+			}
+			if n > 0 {
+				fmt.Println("from:", cAddr.String(), string(buf[:n]))
+			}
+		}
+	}()
+	for {
+		var r, dst string
+		fmt.Scanln(&dst, &r)
+		remoteAddr, e := net.ResolveUDPAddr("udp", dst)
+		if e != nil {
+			panic(e)
+		}
+		n, e := ln.WriteToUDP([]byte(r), remoteAddr)
+		if e != nil {
+			panic(e)
+		}
+		if n > 0 {
+			fmt.Println("write data:", n, r)
+		}
+	}
+}
